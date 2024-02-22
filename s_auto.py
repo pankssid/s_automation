e66aef769e2f03d906e9ad976fee601e05b9ea1a
Pankaj Yadav <33963123+pankssid@users.noreply.github.com>
2024-02-23 03:39:01 +0530

    Update s_auto.py

--- s_auto.py
+++ s_auto.py
@@ -3,6 +3,14 @@ import pandas as pd
 from datetime import datetime
 import base64
 
+# Tracking URLs dictionary
+tracking_urls = {
+    "amazon": "https://track.amazon.in/tracking/{}?trackingId={}",
+    "expressbess": "https://www.xpressbees.com/shipment/tracking?awbNo={}",
+    "ecom": "https://ecomexpress.in/tracking/?awb_field={}",
+    "delhivery": "https://www.delhivery.com/track/package/{}"
+}
+
 def main():
     st.title("File Upload and Column Selection App")
 
@@ -18,11 +26,22 @@ def main():
         # Display the original DataFrame
         st.write("Original DataFrame:")
         st.write(df)
+        
+        df["trackurl"] = df.apply(lambda row: get_tracking_url(row), axis=1)
+
+        df.rename(columns={"Shiprocket Created At": "Order Created At"}, inplace=True)
+        
+        # print(df.columns)
+        # predefined_columns = [
+        #     "Order ID",  "Order Created At", "Status", "Customer Name",
+        #     "Customer Mobile", "Payment Method", "Order Total", "AWB Code",
+        #     "EDD", "Delayed Reason", "Order Delivered Date","trackurl"
+        # ]
+
         predefined_columns = [
-            "Order ID", "Shiprocket Created At", "Status", "Customer Name",
+            "Order ID", "Order Created At", "Status", "Customer Name",
             "Customer Mobile", "Payment Method", "Order Total", "AWB Code",
-            "EDD", "Delayed Reason", "Order Delivered Date"
-        ]
+            "EDD", "Delayed Reason", "Order Delivered Date","trackurl"]
 
         
         # Allow users to select columns to keep
@@ -31,14 +50,9 @@ def main():
         # Create a new DataFrame with selected columns
         selected_df = df[predefined_columns]
 
-        selected_df.columns = [
-            "Order ID",  "Created At", "Status", "Customer Name",
-            "Customer Mobile", "Payment Method", "Order Total", "AWB Code",
-            "EDD", "Delayed Reason", "Order Delivered Date"
-        ]
-
         selected_df=selected_df[~selected_df.Status.isin(['CANCELED'])].reset_index(drop=True)
         
+
         # Display the modified DataFrame
         st.write("DataFrame with selected columns:")
         st.write(selected_df)
@@ -55,5 +69,26 @@ def get_csv_download_link(df):
     href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV</a>'
     return href
 
+def get_tracking_url(row):
+    # Get the tracking URL based on the first word of the "Courier Company" column
+    try:
+        courier_name = row["Courier Company"].lower().split()[0]
+        # print(courier_name)
+    except:
+        courier_name = None
+        return None
+    # print(courier_name,"999999999")
+    if courier_name in tracking_urls:
+        awb_code = row["AWB Code"]
+        if courier_name=='amazon':
+            return tracking_urls[courier_name].format(awb_code,awb_code)
+        else:
+            return tracking_urls[courier_name].format(awb_code)
+    else:
+        return None
+    
+
 if __name__ == "__main__":
     main()
+
+
